4137084d0a98d79e2b62637c5505bca19865625f
Gxl <zdguoxlong@qq.com>
2020-11-08 19:59:12 +0800

    dueling dqn

--- utils_model.py
+++ utils_model.py
@@ -3,15 +3,17 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class DQN(nn.Module):  # 可以修改网络结构 为duel dqn
+class DQN(nn.Module):  # 修改网络结构 为duel dqn
 
     def __init__(self, action_dim, device):
         super(DQN, self).__init__()
         self.__conv1 = nn.Conv2d(4, 32, kernel_size=8, stride=4, bias=False)
         self.__conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2, bias=False)
         self.__conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1, bias=False)
-        self.__fc1 = nn.Linear(64 * 7 * 7, 512)
-        self.__fc2 = nn.Linear(512, action_dim)
+        self.__fc1 = nn.Linear(64 * 7 * 7, 512)  # 优势函数第一层fc
+        self.__fc2 = nn.Linear(512, action_dim)  # 优势函数第二层fc
+        self.__fc1a = nn.Linear(64 * 7 * 7, 512)  # 值函数第一层fc
+        self.__fc2a = nn.Linear(512, 1)  # 值函数第二层fc
         self.__device = device
 
     def forward(self, x):  # 输入状态x（由连续多个frame构成的stack）
@@ -19,8 +21,11 @@ class DQN(nn.Module):  # 可以修改网络结构 为duel dqn
         x = F.relu(self.__conv1(x))
         x = F.relu(self.__conv2(x))
         x = F.relu(self.__conv3(x))
-        x = F.relu(self.__fc1(x.view(x.size(0), -1)))
-        return self.__fc2(x)  # 返回一个长度为action_dim的tensor，表示状态x下每个可选动作的value 即Q(x,a) 网络参数化的对象
+        advantagex = F.relu(self.__fc1(x.view(x.size(0), -1)))
+        advantage = self.__fc2(advantagex)
+        valuex = F.relu(self.__fc1a(x.view(x.size(0), -1)))
+        value = self.__fc2a(valuex)
+        return value + (advantage - advantage.mean(1, keepdim=True))  # dueling dqn：
 
     @staticmethod
     def init_weights(module):
